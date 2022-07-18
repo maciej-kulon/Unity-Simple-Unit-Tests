3ece085d39e3f8088bd86e787d4829ddf1845d9b
Maciej Kulon <kaczal1992@gmail.com>
2022-07-18 20:03:21 +0200

    Added missing extension methods, to remove unnecessary references

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleTests.Extensions
+{
+    public static class Extensions
+    {
+        public static bool IsEmpty(this object value)
+        {
+            if (value == null)
+                return false;
+            return string.IsNullOrEmpty(value.ToString());
+        }
+
+        public static bool IsNotEmpty(this object value)
+        {
+            if (value == null)
+                return false;
+            return !string.IsNullOrEmpty(value.ToString());
+        }
+    }
+}
